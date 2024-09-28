e327a121f43a831a7f4650fb31b0642a54faaa65
linxz-coder <234692261@qq.com>
2024-09-28 23:54:34 +0800

    cors

--- 原生AJAX/server.js
+++ 原生AJAX/server.js
@@ -58,6 +58,39 @@ app.get('/delay', (request, response) => {
     }, 3000);
 })
 
+// jsonp服务
+app.get('/jsonp-server', (request, response) => {
+    // response.send('console.log("Hello JSONP")');
+    const data = {
+        name: 'JSONP'
+    };
+    let str = JSON.stringify(data);
+    response.end(`handle(${str})`);
+})
+
+// 检测用户是否存在
+app.all('/check-username', (request, response) => {
+    const data = {
+        exist: 1,
+        msg: '用户名已经存在'
+    };
+    // 将数据转化为字符串
+    let str = JSON.stringify(data);
+    // 返回结果
+    response.end(`handle(${str})`);
+})
+
+// cors服务
+app.get('/cors-server', (request, response) => {
+    // 设置响应头，设置允许跨域
+    response.setHeader('Access-Control-Allow-Origin', '*'); //后面的参数指允许的url，*是通配符，表示所有的url都可以访问
+    // response.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+    // 设置响头，允许自定义请求头
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    // 设置响应头，允许请求方法
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.send('Hello CORS');
+})
 
 // 监听端口启动服务
 app.listen(8000, () => {
---
+++ 跨域/JSONP/app.js
@@ -0,0 +1,8 @@
+const data = {
+    name: 'JSONP',
+}
+
+handle(data);
+
+
+
